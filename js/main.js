2305c44a40a223b24ce0c4e0138dc089f88c91d8
César Souza Liedke <cesarsl@gmail.com>
2019-03-18 14:16:41 -0300

    feat: filter to remove older posts than today

--- js/main.js
+++ js/main.js
@@ -30,13 +30,27 @@ function sortResults(arr, prop, asc) {
     return arr;
 }
 
+function filterArray(arr) {
+    var today = new Date().getTime();
+    var returnArr = arr.filter(function (el) {
+        var splitDate = el.dataLimite.split('/');
+        var compDate = new Date(splitDate[2], splitDate[1] - 1, splitDate[0]).getTime();
+        if (compDate >= today) {
+            return el;
+        }
+    });
+    return returnArr;
+}
+
 Handlebars.registerHelper('toLowerCase', function(str) {
     return str.toLowerCase();
 });
 
 $(document).ready(function() {
     $.getJSON('https://api.sheety.co/9dc03903-6ddb-4116-9ccd-6978675aac5b', function(data) {
-        var template = Handlebars.compile($('#item-template').html())
-        $('#content').html(template(sortDates(data, 'dataLimite', true)))
+        var template = Handlebars.compile($('#item-template').html());
+        var sorted = sortDates(data, 'dataLimite', true);
+        var filtered = filterArray(sorted);
+        $('#content').html(template(filtered));
     })
 })
